8af510cc93d4723f2f463642deabe4ed655debc9
Coco42s <60662536+Coco42s@users.noreply.github.com>
2023-11-04 22:33:42 +0100

    update main.py

--- main.py
+++ main.py
@@ -8,22 +8,28 @@ def initialize():
     alphabet_low,alphabet_upp = list(ascii_lowercase),list(ascii_uppercase)
     alphabet_dict = {"lower":alphabet_low,"upper":alphabet_upp}
 
-def alphabet_randomizer(boolvar:bool, num = None, text = None):
+def num_alphabet_randomizer(boolvar:bool, num = None, text = None):
     if num is None and text is None: return alphabet_dict
-    if text is None: 
+    
+    if boolvar == True and num is not None:
+        if num <= len(alphabet_low):
+            return num
+    
+    if text is None:   
         if num == 0: 
             return alphabet_dict
         num = abs(num)
         rand = num*sqrt(randint(1,500))+num**3
         while rand > len(alphabet_low): 
             rand = (rand/sqrt(randint(1,2)*rand))+sqrt(rand//randint(1,20))
-    if num is None:
+    
+    if (num is None) or (num != None and text != None):
         val = 0
+        if num != None and text != None: val += num
         for caractere in text:
             val += ord(caractere)
-        return alphabet_randomizer(val)
-    if bool
-    pass
+        return num_alphabet_randomizer(False, val)
+    return round(rand)
 
 initialize()
-alphabet_randomizer(True, None, "Bonjour")
+print(num_alphabet_randomizer(False, 12, "Bonjour"))
